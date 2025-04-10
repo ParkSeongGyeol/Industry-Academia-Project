f0cb08751337c592fe22873ef4c3aa2d5dbd458c
MAX9714 <kimmk3964@naver.com>
2025-04-11 01:32:19 +0900

    Create SpiritManager.h

---
+++ 08_IA_project/SpiritManager.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct Spirit {
+    std::string id;
+    std::string storage_location;
+    std::string transfer_history;
+    std::string production_date;
+    double alcohol_percentage;
+    double yield_liters;
+    std::string raw_material_ratio;
+    int fermentation_days;
+    std::string first_cut_time;
+    std::string last_cut_time;
+    int distillation_count;
+};
+
+class SpiritManager {
+private:
+    std::vector<Spirit> spirits;
+
+public:
+    void run();
+    void addSpirit();
+    void deleteSpirit();
+    void displaySpirits();
+    void updateSpirit();  
+};
